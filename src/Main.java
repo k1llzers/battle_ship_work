3e59e10e7ae0612bb5e7834cc29471062bc00b2b
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-06-23 13:26:10 +0300

    Initialize main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+
 public class Main {
+    public static int botMode = 1;
+    public static int hardMode = 2;
+    public static int coins = 1000;
+    public static int countOfPidkazka = 0;
+    public static int countOfPechatka = 0;
+    public static int countOfTsarBimba = 0;
+    public static JFrame mainFrame = new JFrame();
+    private static Clip clip;
 }
